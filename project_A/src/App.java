614675932b07d54055831e12026e85895e3de2c9
pedrochunks35 <a26899@sa-miranda.net>
2022-03-20 17:07:26 +0000

    pois

--- project_A/src/App.java
+++ project_A/src/App.java
@@ -2,6 +2,7 @@ import org.jpl7.Query;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //HELLO
         System.out.println("Hello, World!");
         Query q=new Query("consult('t.pl')");
         q.hasSolution();
