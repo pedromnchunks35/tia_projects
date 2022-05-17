699028aca2077fa320eca0506fa986592f31c396
pedrochunks35 <a26899@sa-miranda.net>
2022-05-17 12:55:36 +0100

    commited

--- tia_projects2/src/ops_climbV2.pl
+++ tia_projects2/src/ops_climbV2.pl
@@ -11,15 +11,15 @@ eval(L,E):-
 	E is CA + CD.
 /******************************************************************/
 
-/******************************************************************/ç
+/******************************************************************/
 %ir verificar a solucao
-change(S, SNEW):-
+change(S, SNEW,[Cam1,Cam2]):-
 	%ir buscar todas as solucoes de l1 a l2 cujo preco é inferior a 450
-    findall(X,(caminho(l1,l2,X),custo_alojamento(X,C),C =< 450),L), 
+    findall(X,(caminho(Cam1,Cam2,X),custo_alojamento(X,C),C =< 450),L), 
 	%verificar tamanho de lista
 	length(L,LE),
 	%buscar posi na lista random 
-	random(1,LE,R),
+	random(0,LE,R),
 	%ir buscar elemento atraves dessa posi random 
 	nth0(R,L,SNEW),
 	%verificar se as listas sao diferentes
@@ -28,7 +28,13 @@ change(S, SNEW):-
 
 /******************************************************************/
 %init da funcao hill climbing , passamos solucao inicial , numero de iteracoes , probs e o tipo de func 
-demo:- hill_climbing([l2,l4,l3,l1],[30,1,0,min],S),write(S).
+demo(Cam1,Cam2,Numiter,Verbose,Prob):-
+	findall(X,(caminho(Cam1,Cam2,X),custo_alojamento(X,C),C =< 450),L),  
+	length(L,Lengthsol),
+	Lengthsol >= 2,
+	nth0(0,L,K),
+	hill_climbing(K,[Numiter,Verbose,Prob,min],[Cam1,Cam2],S),
+	write(S).
 /******************************************************************/
 % assumes eval(Solution,Result)
 % assumes change(S1,S2)
@@ -62,16 +68,16 @@ show(_,_,_,_,_,_).
 % hill climbing
 % Prob=0 is pure hill climbing, Prob>0 means Stochastic Hill Climbing
 % S0 is the initial solution, Control is a list with the number of iterations, verbose in console, probability and type of optimization.
-hill_climbing(S0,[Iter,Verbose,Prob,Opt],S1):-
+hill_climbing(S0,[Iter,Verbose,Prob,Opt],[Cam1,Cam2],S1):-
 	eval(S0,E0),
         show(0,Verbose,S0,E0,_,_),
-	hill_climbing(S0,E0,0,Iter,Verbose,Prob,Opt,S1).
+	hill_climbing(S0,E0,0,Iter,Verbose,Prob,Opt,[Cam1,Cam2],S1).
 
-hill_climbing(S,_,Iter,Iter,_,_,_,S).
-hill_climbing(S1,E1,I,Iter,Verbose,Prob,Opt,SFinal):-
-	change(S1,SNew),
+hill_climbing(S,_,Iter,Iter,_,_,_,_,S).
+hill_climbing(S1,E1,I,Iter,Verbose,Prob,Opt,[Cam1,Cam2],SFinal):-
+	change(S1,SNew,[Cam1,Cam2]),
 	best(Prob,Opt,S1,E1,SNew,_,S2,E2),
 	I1 is I+1,
         show(I1,Verbose,S1,E1,S2,E2),
-	hill_climbing(S2,E2,I1,Iter,Verbose,Prob,Opt,SFinal).
+	hill_climbing(S2,E2,I1,Iter,Verbose,Prob,Opt,[Cam1,Cam2],SFinal).
 /******************************************************************/
